3a428c4e02fe7e8d1e7484b5660e009c7b4d8e3c
Boris Slavinskiy <slavinskiyboris@gmail.com>
2023-12-07 22:43:51 +0300

    added dropdown for mobile device

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,25 @@
-/* Этот скрипт использует имена классов theme-menu__button, theme-dark, theme-light и theme-auto;
-еще атрибуты disabled и data-theme. Поэтому их нельзя менять в HTML. */
+function replaceDiv() {
+    if (window.innerWidth < 768) {
+        // Находим элемент, который нужно заменить
+        const oldDiv = document.querySelector('.header__nav');
 
-function changeTheme(theme) {
-  document.documentElement.className = '';
-  document.documentElement.classList.add(`theme-${theme}`);
-  localStorage.setItem('theme', theme);
+        // Создаем новый div
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `<div class="header__dropdown">
+  <button class="header__dropdown_button">Menu</button>
+  <div class="header__dropdown_content">
+    <a class="header__dropdown_link" href="#">Mobile Top Up</a>
+    <a class="header__dropdown_link" href="#">About</a>
+    <a class="header__dropdown_link" href="#">Rate</a>
+    <a class="header__dropdown_link" href="#">Help</a>
+  </div>
+</div>`;
+
+        // Заменяем старый div на новый
+        oldDiv.replaceWith(newDiv);
+    }
 }
 
-(function initTheme() {
-  const theme = localStorage.getItem('theme');
-  if (theme) {
-    changeTheme(theme);
-  }
-})();
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.documentElement;
-  const themeButtons = document.querySelectorAll('.theme-menu__button');
-
-  function setDisabled(theme) {
-    themeButtons.forEach((item) => {
-      if (item.getAttribute('data-theme') === theme) {
-        item.setAttribute('disabled', true);
-      } else {
-        item.removeAttribute('disabled');
-      }
-    });
-  }
-
-  if ([...root.classList].includes('theme-light')) {
-    setDisabled('light');
-  } else if ([...root.classList].includes('theme-dark')) {
-    setDisabled('dark');
-  } else {
-    setDisabled('auto');
-  }
-
-  themeButtons.forEach((button) => {
-    button.onclick = () => {
-      changeTheme(button.getAttribute('data-theme'));
-      setDisabled(button.getAttribute('data-theme'));
-    };
-  });
-});
+// Вызываем функцию при загрузке страницы и при изменении размера окна
+window.onload = replaceDiv;
+window.onresize = replaceDiv;
