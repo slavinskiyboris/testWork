effcd8705429260a1f202f8899032872e5180a03
Boris Slavinskiy <slavinskiyboris@gmail.com>
2023-12-07 22:45:56 +0300

    added dropdown for mobile device

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 function replaceDiv() {
     if (window.innerWidth < 768) {
-        // Находим элемент, который нужно заменить
+
         const oldDiv = document.querySelector('.header__nav');
 
-        // Создаем новый div
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `<div class="header__dropdown">
   <button class="header__dropdown_button">Menu</button>
@@ -15,11 +14,9 @@ function replaceDiv() {
   </div>
 </div>`;
 
-        // Заменяем старый div на новый
         oldDiv.replaceWith(newDiv);
     }
 }
 
-// Вызываем функцию при загрузке страницы и при изменении размера окна
 window.onload = replaceDiv;
 window.onresize = replaceDiv;
